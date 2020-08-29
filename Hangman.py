40cec85ed3b4bd635e810fe97b15482db63fa251
Hello <doston111.matyakubov@gmail.com>
2020-08-29 16:31:09 +0500

    Solution for the third stage of the hangman project - choosing computer answer randomly

--- Hangman.py
+++ Hangman.py
@@ -1,11 +1,13 @@
-# Stage 2/8 of the Hangman project
+# Stage 3/8 of the Hangman project
+import random
 
 print("H A N G M A N")
-answer = "python"
+answer = ('python', 'java', 'kotlin', 'javascript')
 
 word = input("Guess the word: ")
+random_index = random.randint(0, 3)
 
-if answer == word:
+if answer[random_index] == word:
     print("You survived!")
 else:
     print("You are hanged!")
