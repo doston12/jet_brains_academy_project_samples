49755d4583343b7fb21ffafb73398843dec24085
Hello <doston111.matyakubov@gmail.com>
2020-08-30 11:59:12 +0500

    Final solution for the hangmang final project - stage 8/8

--- Hangman.py
+++ Hangman.py
@@ -1,44 +1,62 @@
-# Stage 6/8 of the Hangman project
+# Stage 8/8 of the Hangman project
 import random
 
 print("H A N G M A N")
-answer = ('python', 'java', 'kotlin', 'javascript')
 
-player_lives = 8
-player_letters = []
-random_index = random.randint(0, 3)
-random_answer = answer[random_index]
-hidden_answer = list('-'*len(random_answer))
+while True:
 
-while player_lives > 0:
-    print()
+    player_action = input('Type "play" to play the game, "exit" to quit: ')
 
-    print("".join(hidden_answer))
-    letter = input("Input a letter: ")
+    if player_action == "play":
 
-    if letter in player_letters:
-        print("No improvements")
-        player_lives -= 1
+        player_lives = 8
+        answer = ('python', 'java', 'kotlin', 'javascript')
+        player_letters = []
+        random_index = random.randint(0, 3)
+        random_answer = answer[random_index]
+        hidden_answer = list('-' * len(random_answer))
+
+        while player_lives > 0:
+            print()
+            print("".join(hidden_answer))
+            letter = input("Input a letter: ")
+
+            if len(letter) != 1:
+                print("You should input a single letter")
+                continue
+
+            if not(ord(letter) >= 97 and ord(letter) <= 122):
+                print("It is not an ASCII lowercase letter")
+
+            else:
+
+                if letter in player_letters:
+                    print("You already typed this letter")
+
+                else:
+
+                    player_letters.append(letter)
+
+                    if letter in random_answer:
+                        indexes = []
+                        for char_index in range(len(random_answer)):
+                            if letter == random_answer[char_index]:
+                                indexes.append(char_index)
+
+                        for index in indexes:
+                            hidden_answer[index] = letter
+
+                    else:
+                        player_lives -= 1
+                        print("No such letter in the word")
+
+        if player_lives <= 0:
+            print("You are hanged!")
+        else:
+            print("You survived!")
+            print("You guessed the word!")
 
     else:
+        break
 
-        if letter in random_answer:
-            indexes = []
-            for char_index in range(len(random_answer)):
-                if letter == random_answer[char_index]:
-                    indexes.append(char_index)
-
-            for index in indexes:
-                hidden_answer[index] = letter
-            player_letters.append(letter)
-
-        else:
-            player_lives -= 1
-            print("No such letter in the word")
-
-if player_lives <= 0:
-    print("You are hanged!")
-else:
-    print("You survived!")
-    print("You guessed the word!")
 
