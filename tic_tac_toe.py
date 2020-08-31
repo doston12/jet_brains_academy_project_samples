22984046e6fcf59d7dd1801002cec40afddf8ce0
Hello <doston111.matyakubov@gmail.com>
2020-08-31 06:08:08 +0500

    Solution for the stage 2/5 of the Tic Tac Toe project

---
+++ tic_tac_toe.py
@@ -0,0 +1,11 @@
+# stage 2/5 of the Tic Tac Toe project
+
+user_input = input("Enter cells: ")
+
+print("---------")
+
+print('|', user_input[0], user_input[1], user_input[2], '|')
+print('|', user_input[3], user_input[4], user_input[5], '|')
+print('|', user_input[6], user_input[7], user_input[8], '|')
+
+print("---------")
