851d744712b01b57eb1736d8d68176e64f06383c
doston12 <doston111.matyakubov@gmail.com>
2020-09-12 16:17:13 +0500

    Solution for 4th stage

--- numeric matrix processor.py
+++ numeric matrix processor.py
@@ -1,5 +1,4 @@
-# Solution for stage 3/6 of Numeric matrix processor project
-# new comment line
+# Solution for stage 4/6 of Numeric matrix processor project
 
 def init_matrix(rows):
     matrix = []
@@ -11,6 +10,7 @@ def init_matrix(rows):
     return matrix
 
 def print_matrix(matrix, rows, cols):
+    print("The result is: ")
     for i in range(rows):
         for j in range(cols):
             print(matrix[i][j], end=' ')
@@ -47,17 +47,52 @@ def multiply_matricies(matrix1, rows1, cols1, matrix2, cols2):
 
     return res_matrix
 
+def transpose_by_main_diagonal(matrix, rows, cols):
+    res_matrix = [ [ 0 for i in range(cols) ] for j in range(rows) ]
+
+    for i in range(rows):
+        for j in range(cols):
+            res_matrix[i][j] = matrix[j][i]
+
+    print_matrix(res_matrix, rows, cols)
+
+def transpose_by_side_diagonal(matrix, rows, cols):
+    res_matrix = [[0 for i in range(cols)] for j in range(rows)]
+    for i in range(rows):
+        for j in range(cols):
+            res_matrix[i][j] = matrix[rows-j-1][cols-i-1]
+
+    print_matrix(res_matrix, rows, cols)
+
+def transpose_by_vertical_line(matrix, rows, cols):
+    res_matrix = [[0 for i in range(cols)] for j in range(rows)]
+    for i in range(rows):
+        for j in range(cols):
+            res_matrix[i][j] = matrix[i][cols-j-1]
+
+    print_matrix(res_matrix, rows, cols)
+
+def transpose_by_horizontal_line(matrix, rows, cols):
+    res_matrix = [[0 for i in range(cols)] for j in range(rows)]
+    for i in range(cols):
+        for j in range(rows):
+            res_matrix[i][j] = matrix[cols-i-1][j]
+
+    print_matrix(res_matrix, rows, cols)
+
 def print_menu():
     print("1. Add matrices")
     print("2. Multiply matrix by a constant")
     print("3. Multiply matrices")
+    print("4. Transpose matrix")
     print("0. Exit")
+
 
 
 while True:
     print_menu()
     user_option = int(input("Your choice: "))
-
+    print()
 
     if user_option == 1:
         matrix1_rows, matrix1_cols = map(int, input("Enter size of first matrix: ").split(' '))
@@ -97,6 +132,27 @@ while True:
             print("The operation cannot be performed.")
             print()
 
+    elif user_option == 4:
+        print("1. Main diagonal")
+        print("2. Side diagonal")
+        print("3. Vertical line")
+        print("4. Horizontal line")
+        user_choice = int(input("Your choice: "))
+
+        matrix1_rows, matrix1_cols = map(int, input("Enter matrix size: ").split(' '))
+        print("Enter matrix: ")
+
+        matrix1 = init_matrix(matrix1_rows)
+
+        if user_choice == 1:
+            transpose_by_main_diagonal(matrix1, matrix1_rows, matrix1_cols)
+        elif user_choice == 2:
+            transpose_by_side_diagonal(matrix1, matrix1_rows, matrix1_cols)
+        elif user_choice == 3:
+            transpose_by_vertical_line(matrix1, matrix1_rows, matrix1_cols)
+        elif user_choice == 4:
+            transpose_by_horizontal_line(matrix1, matrix1_rows, matrix1_cols)
+
+
     elif user_option == 0:
         break
-
