d9fccccf3e75d2fab771dbc84d4916e0d17a8f34
doston12 <doston111.matyakubov@gmail.com>
2020-10-19 14:14:21 +0200

    solution for stage 3

--- regex_engine.py
+++ regex_engine.py
@@ -1,18 +1,35 @@
-# regex engine project - stage 1/6
+# write your code here
+import sys
+sys.setrecursionlimit(10000)
 
-def single_char_comparasion(s):
-    index = s.find('|')
-    if index == 0 or index == -1:
+def check_regex(re, ch):
+    return True if re == '' or re == '.' or re == ch else False
+
+def check_whole(regex, word):
+    if not regex:
         return True
-    elif index == 1 and len(s) == 2:
+    elif not word:
+        return False
+    elif len(word) == 1:
+        return check_regex(regex, word)
+    elif check_regex(regex[0], word[0]):
+        return check_whole(regex[1:], word[1:])
+    else:
+        return False
+
+def check_sub(regex, word):
+    if len(regex) > len(word):
         return False
     else:
-        if s[0] == '.':
-            return True
-        if s[0] == s[2]:
-            return True
+        temp = True
+        for i in range(0, len(regex)):
+            if regex[i] != '.' and regex[i] != word[i]:
+                temp = False
+                break
+        if not temp:
+            return check_sub(regex, word[1:])
         else:
-            return False
+            return temp
 
-
-print(single_char_comparasion(input("Input: ")))
+regex, word = input().split('|')
+print(check_sub(regex, word))
